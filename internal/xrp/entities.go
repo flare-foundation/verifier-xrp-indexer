eb171f2c7a08f324546f3621d8b9b723167d419a
Ryan Collingham <ryan.collingham@flare.network>
2025-11-26 11:32:57 +0000

    use local framework and implement required new methods for history drop

--- internal/xrp/entities.go
+++ internal/xrp/entities.go
@@ -1,10 +1,33 @@
 package xrp
 
+import "github.com/flare-foundation/verifier-indexer-framework/pkg/database"
+
 type Block struct {
 	Hash         string `gorm:"primaryKey;type:varchar(64)"`
 	BlockNumber  uint64 `gorm:"index"`
 	Timestamp    uint64 `gorm:"index"`
 	Transactions uint64
+}
+
+func (b Block) GetBlockNumber() uint64 {
+	return b.BlockNumber
+}
+
+func (b Block) GetTimestamp() uint64 {
+	return b.Timestamp
+}
+
+func (b Block) TimestampQuery() string {
+	return "timestamp < ?"
+}
+
+func (b Block) HistoryDropOrder() []database.Deletable {
+	// It does not particularly matter the order as there are no foreign key constraints.
+	// However we drop transactions first to avoid leaving orphaned records in case of partial failures.
+	return []database.Deletable{
+		Transaction{},
+		Block{},
+	}
 }
 
 type Transaction struct {
@@ -17,10 +40,6 @@ type Transaction struct {
 	SourceAddressesRoot string `gorm:"index;type:varchar(64);default:null"`
 }
 
-func (b Block) GetBlockNumber() uint64 {
-	return b.BlockNumber
-}
-
-func (b Block) GetTimestamp() uint64 {
-	return b.Timestamp
+func (t Transaction) TimestampQuery() string {
+	return "timestamp < ?"
 }
